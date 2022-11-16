3763547bb4c528a607ffa21d206fe0fbac311a57
Mike Rae <mikerae@me.com>
2022-11-16 22:09:01 +0000

    Add post_detail templates, view and url path

--- codestar/settings.py
+++ codestar/settings.py
@@ -97,6 +97,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa E501
